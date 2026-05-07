ec631f5a6146ec9ba2d8a7977cb899221b256c05
小陆 <527528007@qq.com>
2026-05-07 09:12:16 +0800

    feat: 1 - 1

---
+++ src/apps/com.ss.android.ugc.aweme.ts
@@ -0,0 +1,33 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'com.ss.android.ugc.aweme',
+  name: '抖音',
+  groups: [
+    {
+      key: 30,
+      name: '功能类-短剧自动上滑',
+      desc: '检测到“上滑继续观看短剧”或“返回并退出”时自动上滑',
+      rules: [
+        {
+          matches: [
+            'TextView[text="上滑继续观看短剧"]',
+            'TextView[text="返回并退出"]',
+          ],
+          action: 'swipe',
+          swipeArg: {
+            start: { x: 'width * 0.5', y: 'height * 0.82' },
+            end: { x: 'width * 0.5', y: 'height * 0.25' },
+            duration: 300,
+          },
+          fastQuery: true,
+          matchTime: 10000,
+          resetMatch: 'app',
+          activityIds: [
+            'com.ss.android.ugc.aweme.playlet.videodetail.PlayletVideoPlayActivity',
+          ],
+        },
+      ],
+    },
+  ],
+});
